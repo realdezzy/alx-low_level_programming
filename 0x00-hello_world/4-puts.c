98d01e961014ffc13d27c12dd97a55cced5a19ac
realdezzy <realdezzy1@gmail.com>
2022-09-09 07:57:23 -0700

    Betty corrections

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
-
-int main(){
-	/*
-	 *Print string to stdout with put
-	 * */
+/**
+ * main - Prints string to stdout using put
+ * Return: 0 is returned
+ */
+int main(void)
+{
 	char myChar[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(myChar);
 	return (0);
+
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
-
-int main()
+/**
+* main - Yeah its main
+* Return: 0 is returned
+*/
+int main(void)
 {
 	/*
-	 * Print text to standard output using printf
-	 *
-	* */
+	*
+	* Print text to standard output using printf
+	*/
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
