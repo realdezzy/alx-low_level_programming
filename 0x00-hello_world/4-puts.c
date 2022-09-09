81522385c101368d5bc857bd9d2b2ef840fc4a55
realdezzy <realdezzy1@gmail.com>
2022-09-08 21:55:36 -0700

    Cprogramming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+	/*
+	 *Print string to stdout with put
+	 * */
+	char myChar[] = "\"Programming is like building a multilingual puzzle";
+	puts(myChar);
+	return 0;
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+	/*
+	 *Print text to standard output using printf
+	 * */
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return 0;
+}
