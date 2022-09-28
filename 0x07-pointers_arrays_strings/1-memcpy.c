f794d9d737d3b77d953c1a12e757098d26913b41
realdezzy <realdezzy1@gmail.com>
2022-09-27 17:25:07 -0700

    Little experiment

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,11 +11,12 @@ include "main.h"
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i;
+	unsigned int i;
 
-	for (i = 0;i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
+
 	}
 
 	return (dest);
