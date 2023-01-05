de0aa319040936b89151619b37ed927e916c3452
realdezzy <realdezzy1@gmail.com>
2023-01-05 09:54:07 -0800

    Create

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+
+/**
+  * hash_table_create - Creates a hash table
+  * @size: size of the hash table
+  *
+  * Return: Pointer to newly created hash table or NULL
+  */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_table;
+
+	new_table = malloc(sizeof(hash_table_t) * size);
+
+	if (new_table == NULL)
+		return (NULL);
+
+	return (new_table);
+}
