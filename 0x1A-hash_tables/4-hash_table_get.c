78a5bcd4e135968f1335ca363730fd302691f2f0
realdezzy <realdezzy1@gmail.com>
2023-01-06 17:58:53 -0800

    correction

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,8 +13,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *temp;
 	unsigned long int index;
 
-	if (!ht || !key || !*key || !value)
-		return (0);
+	if (!ht || !key || !*key)
+		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
 	temp = ht->array[index];
 
