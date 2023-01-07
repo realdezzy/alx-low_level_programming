ea2d9b9560d340af039042ae4f45005a98f13859
realdezzy <realdezzy1@gmail.com>
2023-01-06 17:56:37 -0800

    betty 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,6 +13,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *temp;
 	unsigned long int index;
 
+	if (!ht || !key || !*key || !value)
+		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	temp = ht->array[index];
 
@@ -21,7 +23,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 
 	else
 	{
-		while(temp)
+		while (temp)
 		{
 			if (strcmp(temp->key, key) == 0)
 				return (temp->value);
